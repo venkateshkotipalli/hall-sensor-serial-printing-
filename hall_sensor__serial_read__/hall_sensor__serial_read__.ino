9f4ae4d42fcbb8e361e0de2e3dbf4e480e59a342
venkateshkotipalli <48756118+venkateshkotipalli@users.noreply.github.com>
2019-03-23 12:53:04 +0530

    Add files via upload

---
+++ hall_sensor__serial_read__/hall_sensor__serial_read__.ino
@@ -0,0 +1,25 @@
+int hall;
+int hall2;
+void setup() {
+
+      Serial.begin(9600);
+      pinMode(7,INPUT);
+}
+
+void loop() {
+              Serial.read();
+              hall= digitalRead(7);
+              if(hall==HIGH)
+              {
+                Serial.print("No Magnetic field then output of the hall sensor is one(1)");
+                Serial.println(hall);
+                delay(500);
+              }
+              if(hall==LOW)
+              {
+                Serial.print(" Magnetic field output of the sensor is zero(0)");
+                Serial.println(hall);
+                delay(500);
+              }
+            }
+
